0bf9224ef18bd2048b4e532ebf1ed970edd49db5
Shan Shaji <developer.shanshaji@gmail.com>
2021-07-16 02:01:41 +0530

    feat: Add introduction method

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 // Third party modules
 import arg from 'arg';
@@ -7,86 +7,64 @@ import envinfo from 'envinfo';
 import leven from 'leven';
 import chalk from 'chalk';
 
-import { init } from './commands/init/index';
+import init from './commands/init/index';
 import * as prompts from './utils/prompt';
 import * as logger from './utils/logger';
 import { commander } from './helpers/commander';
 
-import pkg from '../package'
+import pkg from '../package';
 import { program } from 'commander';
-
-
-
 
 updateNotifier({ pkg }).notify();
 
 const suggestCommands = (cmd) => {
-    const availableCommands = commander.commands.map((c) => c._name, );
+  const availableCommands = commander.commands.map((c) => c._name);
 
-    const suggestion = availableCommands.find((c) => leven(c, cmd) < c.length * 0.4, );
+  const suggestion = availableCommands.find(
+    (c) => leven(c, cmd) < c.length * 0.4
+  );
 
-    if (suggestion) {
-        logger.error(` Did you mean ${chalk.yellow(suggestion)}?`);
-    }
-}
+  if (suggestion) {
+    logger.error(` Did you mean ${chalk.yellow(suggestion)}?`);
+  }
+};
 
 commander.version(pkg.version).usage('<command> [options]');
 
-
-commander.command('init <appname>')
-    .description('Scaffolds a Flutter project in the current path')
-    .action(init);
-
+commander
+  .command('init <appname>')
+  .description('Scaffolds a Flutter project in the current path')
+  .action(init);
 
 commander
-    .command('info')
-    .description('Prints debugging information about the local environment')
-    .action(() => {
-        console.log(chalk.bold('\nEnvironment Info:'));
-        envinfo
-            .run({
-                System: ['OS', 'CPU'],
-                Binaries: ['Node', 'Yarn', 'npm'],
-                Browsers: ['Chrome', 'Edge', 'Firefox', 'Safari'],
-                npmGlobalPackages: ['arkroot-cli'],
-            })
-            .then(console.log);
-    });
-
+  .command('info')
+  .description('Prints debugging information about the local environment')
+  .action(() => {
+    console.log(chalk.bold('\nEnvironment Info:'));
+    envinfo
+      .run({
+        System: ['OS', 'CPU'],
+        Binaries: ['Node', 'Yarn', 'npm'],
+        Browsers: ['Chrome', 'Edge', 'Firefox', 'Safari'],
+        npmGlobalPackages: ['arkroot-cli'],
+      })
+      .then(console.log);
+  });
 
 // Validation for unknown commands
 commander.on('command:*', ([cmd]) => {
-    commander.outputHelp();
-    logger.error(`\n Unknown command ${chalk.yellow(cmd)}.\n`);
-    suggestCommands(cmd);
-    process.exitCode = 1;
+  commander.outputHelp();
+  logger.error(`\n Unknown command ${chalk.yellow(cmd)}.\n`);
+  suggestCommands(cmd);
+  process.exitCode = 1;
 });
-
 
 commander.parse(process.argv);
 
-
 // Shows up help if no arguments were provided.
 if (!commander.args.length) {
-    commander.help();
+  commander.help();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // export const parseArgumentsIntoOptions = (rawArgs) => {
 //   const args = arg(
--- src/commands/init/index.js
+++ src/commands/init/index.js
@@ -6,10 +6,12 @@ import path from 'path';
 import { promisify } from 'util';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import figlet from 'figlet';
 import Listr from 'listr';
 
 import * as gitHelpers from '../git/index';
 import * as filehelpers from '../../utils/filesHandling';
+import * as logger from '../../utils/logger';
 
 let projectPathRelative;
 
@@ -22,11 +24,39 @@ const access = promisify(fs.access);
  */
 
 const intialCommit = () => {
-    // Commands to be executed serially
-    const commands = ['init', 'add.', `commitn -m "Init" -m "ARKROOT-CLI"`];
-    gitHelpers.executeByOrder(commands, projectPathRelative);
+  // Commands to be executed serially
+  const commands = ['init', 'add.', `commitn -m "Init" -m "ARKROOT-CLI"`];
+  gitHelpers.executeByOrder(commands, projectPathRelative);
 };
 
-export const init = async(options) => {
-    console.log(options);
+/**
+ * Shows application name
+ *
+ * @param {msg} msg - App name
+ * @return {void} -  returns nothing
+ */
+
+const showIntroduction = async (msg) => {
+  console.log(
+    figlet.textSync(msg, {
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      whitespaceBreak: true,
+    })
+  );
+};
+
+/**
+ * Scaffolds flutter based app
+ *
+ * @param appName - specifies app name
+ */
+
+export default async (appName) => {
+  showIntroduction(`Arkroot-Cli`);
+
+  const argument = process.argv[4];
+
+  const hasMultipleProjectNameArguments = argument && !argument.startsWith('-');
+  console.log(hasMultipleProjectNameArguments);
 };
