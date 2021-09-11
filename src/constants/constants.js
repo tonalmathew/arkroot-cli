6d78c8e6c3b8a3ba36e8c87f71c62dc12bf5b58f
shan-shaji <abrahamshanshaji@gmail.com>
2021-09-11 22:38:35 +0530

    feat: add deps validate

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,5 @@
+('use strict');
+
 /**
  * @type {String} appName - App Name
  * @type {String} tagLine - Tagline for the app
@@ -9,3 +11,8 @@ export const tagLine = 'Faster way to setup your projects 🚀\n';
 
 export const defaultTemplate = 'Flutter';
 export const defaultProjectName = 'Project';
+
+// Detecting host OS.
+export const isWin = process.platform === 'win32';
+export const isLinux = process.platform === 'linux';
+export const isMac = process.platform === 'darwin';
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,6 +1,40 @@
 'use strict';
 
 import execa from 'execa';
+import inquirer from 'inquirer';
+import { isLinux, isWin } from '../constants/constants';
+import Spinner from './spinner';
+
+const spinner = Spinner();
+
+/**
+ * Shows installation information
+ *
+ * @param {String} depCandidate - The repective package to be installed
+ * @param {String} url - Official downloads page url
+ * @returns {Void}
+ */
+
+const showInstallationInfo = (depCandidate, url) => {
+  const msg = ` You need to download ${depCandidate} from the official downloads page: ${url}`;
+  console.log(chalk.cyan.bold(msg));
+  process.exit(0);
+};
+
+/**
+ *
+ * Install Git for supported platforms, else show installation instructions
+ * @returns {Promise<void>}
+ */
+
+const installGit = () => {
+  const url = 'https://git-scm.com/download/win';
+  if (isWin) {
+    return showInstallationInfo('git', url);
+  }
+  const packageMgr = isLinux ? 'apt' : 'brew';
+  return exec(`${packageMgr} install git`);
+};
 
 /**
  *
@@ -22,5 +56,32 @@ export const validateInstallation = async (dependency) => {
   if (dependency.includes(' ')) {
     const sep = dependency.includes('-') ? '-' : '';
     dependency = dependency.split(sep)[0];
+  }
+
+  if (!isDepInstalled) {
+    const { shouldInstallDep } = await inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'shouldInstallDep',
+        message: `Sorry, ${dependency} is not installed on your system, Do you want to install it?`,
+      },
+    ]);
+
+    if (!shouldInstallDep) {
+      console.warn(
+        chalk.yellow.bold(` Warning:- ${chalk.cyan.bold(
+          `${dependency} is required to be installed`
+        )}
+        `)
+      );
+      process.exit(1);
+    }
+
+    spinner.text = `Installing ${dependency}`;
+    spinner.start();
+
+    if (dependency === 'git') {
+      return installGit();
+    }
   }
 };
